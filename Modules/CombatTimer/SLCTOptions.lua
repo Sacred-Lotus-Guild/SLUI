50d1bc887efb3ca05a6223290e926b4cec306da2
Tim Summers <tasummer32@gmail.com>
2026-02-17 09:34:10 -0500

    Rename and prep for options

---
+++ Modules/CombatTimer/SLCTOptions.lua
@@ -0,0 +1,11 @@
+local SLUI = select(2,...)
+
+SLUI.options.args.timer = {
+    name = "Combat Timer",
+    type = "group",
+}
+local timerOptions {
+    
+}
+
+SLUI.options.args.timer.args = timerOptions
