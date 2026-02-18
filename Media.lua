72c34ca5e593b449450a8b1942082726ab6cbbb0
Griffin Yourick <gryphon92@gmail.com>
2026-02-18 15:02:34 -0500

    late 12.0 support

--- Media.lua
+++ Media.lua
@@ -5,18 +5,13 @@ local SharedMedia = LibStub("LibSharedMedia-3.0")
 
 SLUI.logo = [[Interface\AddOns\SLUI\Media\Textures\logo.blp]]
 SLUI.breakImages = {
-    common = {
-        [[Interface\AddOns\SLUI\Media\Textures\HorseBio.tga]],
-    },
-    group1 = {
-        [[Interface\AddOns\SLUI\Media\Textures\CalemKnee.tga]],
-        [[Interface\AddOns\SLUI\Media\Textures\ForThomas.tga]],
-        [[Interface\AddOns\SLUI\Media\Textures\Priory.tga]],
-        [[Interface\AddOns\SLUI\Media\Textures\StripperBio.tga]],
-        [[Interface\AddOns\SLUI\Media\Textures\WerthersOriginal.tga]],
-        [[Interface\AddOns\SLUI\Media\Textures\Wolf.tga]],
-    },
-    group2 = {}
+    [[Interface\AddOns\SLUI\Media\Textures\CalemKnee.tga]],
+    [[Interface\AddOns\SLUI\Media\Textures\ForThomas.tga]],
+    [[Interface\AddOns\SLUI\Media\Textures\HorseBio.tga]],
+    [[Interface\AddOns\SLUI\Media\Textures\Priory.tga]],
+    [[Interface\AddOns\SLUI\Media\Textures\StripperBio.tga]],
+    [[Interface\AddOns\SLUI\Media\Textures\WerthersOriginal.tga]],
+    [[Interface\AddOns\SLUI\Media\Textures\Wolf.tga]],
 }
 
 SharedMedia:Register("sound", "|cff00ff98Awoo|r", [[Interface\AddOns\SLUI\Media\Sounds\Awoo.ogg]])
