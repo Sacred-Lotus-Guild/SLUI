521bedb072cf6300c81a5ef72244a72d48518092
Griffin Yourick <gryphon92@gmail.com>
2026-03-27 11:25:47 -0400

    InviteTools updates for secret value handling

--- Modules/InviteTools.lua
+++ Modules/InviteTools.lua
@@ -85,31 +85,25 @@ SLUI.options.args.invite = {
     }
 }
 
-local function IsRaidCombatRestricted()
-    return C_RestrictedActions.IsAddOnRestrictionActive(
-        Enum.AddOnRestrictionType.Encounter
-    )
-end
-
 --- Cache a list of guild members that should be promoted based on the configured
 --- minimum guild rank.
-function InviteTools:CacheGuildMembers()
-    self.promoteGuildMembers = {}
+function InviteTools:GUILD_ROSTER_UPDATE()
+    if InCombatLockdown() or not IsInGuild() then return end
+    wipe(self.promoteGuildMembers)
 
-    if IsInGuild() then
-        C_GuildInfo.GuildRoster()
+    -- Request updated guild roster information from the server.
+    C_GuildInfo.GuildRoster()
 
-        local numGuildMembers = GetNumGuildMembers() or 0
-        for i = 1, numGuildMembers do
-            local name, _, rankIndex = GetGuildRosterInfo(i)
+    local numGuildMembers = GetNumGuildMembers() or 0
+    for i = 1, numGuildMembers do
+        local name, _, rankIndex = GetGuildRosterInfo(i)
 
-            -- The `rankIndex` returned is 0-indexed while guildRank from
-            -- `GuildControlGetRankName` is 1-indexed, so we use < rather than
-            -- <= here to compare them.
-            -- see: https://warcraft.wiki.gg/wiki/API_GetGuildRosterInfo
-            if name and rankIndex < SLUI.db.global.invite.guildRank then
-                tinsert(self.promoteGuildMembers, Ambiguate(name, "none"))
-            end
+        -- The `rankIndex` returned is 0-indexed while guildRank from
+        -- `GuildControlGetRankName` is 1-indexed, so we use < rather than
+        -- <= here to compare them.
+        -- see: https://warcraft.wiki.gg/wiki/API_GetGuildRosterInfo
+        if name and rankIndex < SLUI.db.global.invite.guildRank then
+            tinsert(self.promoteGuildMembers, Ambiguate(name, "none"))
         end
     end
 end
@@ -123,30 +117,22 @@ end
 
 --- Returns true if the player will suggest invite instead of invite themselves.
 function WillSuggestInvite()
-    return IsInGroup() and not (
-        UnitIsGroupLeader("player") or UnitIsGroupAssistant("player")
-    )
+    return IsInGroup() and not (UnitIsGroupLeader("player") or UnitIsGroupAssistant("player"))
 end
 
 --- Handle whispers for invite keywords.
 function InviteTools:CHAT_MSG_WHISPER(_, text, playerName)
-    if IsRaidCombatRestricted() or WillSuggestInvite() then
-        return
-    end
+    if WillSuggestInvite() or issecretvalue(text) or issecretvalue(playerName) then return end
 
     if tContains(SLUI.db.global.invite.keywords, text:trim():lower()) then
         self:CheckAndConvertToRaid();
-
-        local unit = Ambiguate(playerName, "none")
-        C_PartyInfo.InviteUnit(unit)
+        C_PartyInfo.InviteUnit(Ambiguate(playerName, "none"))
     end
 end
 
 --- Handle Battle.net whispers for invite keywords.
 function InviteTools:CHAT_MSG_BN_WHISPER(_, text, _, _, _, _, _, _, _, _, _, _, _, bnSenderID)
-    if IsRaidCombatRestricted() or WillSuggestInvite() then
-        return
-    end
+    if WillSuggestInvite() or issecretvalue(text) or issecretvalue(bnSenderID) then return end
 
     if tContains(SLUI.db.global.invite.keywords, text:trim():lower()) then
         local accountInfo = C_BattleNet.GetAccountInfoByID(bnSenderID)
@@ -164,9 +150,7 @@ end
 ---@param unit string
 ---@return boolean
 function InviteTools:ShouldPromote(unit)
-    if self.demotedPlayers[UnitName(unit)] then
-        return false
-    end
+    if issecretvalue(unit) or self.demotedPlayers[UnitName(unit)] then return false end
 
     local name = Ambiguate(unit, "none")
     return tContains(self.promoteGuildMembers, name) or tContains(SLUI.db.global.invite.characters, name)
@@ -175,26 +159,19 @@ end
 --- Auto-promote players to assistant when they join the raid if they are in the
 --- configured list of characters or have the required guild rank.
 function InviteTools:GROUP_ROSTER_UPDATE()
-    if IsRaidCombatRestricted() or not IsInRaid() or not UnitIsGroupLeader("player") then
-        return
-    end
+    if not IsInRaid() or not UnitIsGroupLeader("player") then return end
 
     -- It's discouraged to use GetNumGroupMembers() since there can be "holes"
     -- between raid1 to raid40.
     -- see: https://warcraft.wiki.gg/wiki/API_GetRaidRosterInfo
     for i = 1, MAX_RAID_MEMBERS do
-        local unit, rank = GetRaidRosterInfo(i)
-        if unit and rank < 1 then
-            if self:ShouldPromote(unit) then
-                PromoteToAssistant(unit)
+        local name, rank = GetRaidRosterInfo(i)
+        if name and rank < 1 then
+            if self:ShouldPromote(name) then
+                PromoteToAssistant(name)
             end
         end
     end
-end
-
---- Clear the list of demoted players when joining/forming/leaving a group.
-function InviteTools:ClearDemotedPlayers()
-    wipe(self.demotedPlayers)
 end
 
 function InviteTools:OnInitialize()
@@ -204,20 +181,23 @@ function InviteTools:OnInitialize()
 end
 
 function InviteTools:OnEnable()
-    self:CacheGuildMembers()
-    self:ClearDemotedPlayers()
-    self:RegisterEvent("GUILD_ROSTER_UPDATE", "CacheGuildMembers")
+    self:GUILD_ROSTER_UPDATE()
+    wipe(self.demotedPlayers)
 
+    self:RegisterEvent("GUILD_ROSTER_UPDATE")
     self:RegisterEvent("CHAT_MSG_WHISPER")
     self:RegisterEvent("CHAT_MSG_BN_WHISPER")
     self:RegisterEvent("GROUP_ROSTER_UPDATE")
-    self:RegisterEvent("GROUP_JOINED", "ClearDemotedPlayers")
-    self:RegisterEvent("GROUP_FORMED", "ClearDemotedPlayers")
-    self:RegisterEvent("GROUP_LEFT", "ClearDemotedPlayers")
+    self:RegisterEvent("GROUP_JOINED", function() wipe(self.demotedPlayers) end)
+    self:RegisterEvent("GROUP_FORMED", function() wipe(self.demotedPlayers) end)
+    self:RegisterEvent("GROUP_LEFT", function() wipe(self.demotedPlayers) end)
 
     self:SecureHook("DemoteAssistant", function(unit)
-        if unit then
-            self.demotedPlayers[UnitName(unit)] = true
+        if unit and not issecretvalue(unit) then
+            local name = UnitName(unit)
+            if name then
+                self.demotedPlayers[name] = true
+            end
         end
     end)
 end
