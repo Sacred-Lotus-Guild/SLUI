44d546b4f5bb5bf9b585abc21bc8245be09b5435
antoine brouillette <antoineqc@hotmail.com>
2026-08-14 17:47:17 -0400

    Added one more image

--- Media/Media.lua
+++ Media/Media.lua
@@ -20,6 +20,7 @@ SLUI.breakImages = {
     [[Interface\AddOns\SLUI\Media\Textures\BioWorm.tga]],
     [[Interface\AddOns\SLUI\Media\Textures\BioCarpet.tga]],
     [[Interface\AddOns\SLUI\Media\Textures\xalatath.tga]],
+    [[Interface\AddOns\SLUI\Media\Textures\ShcorpAfk.tga]],
 }
 
 SharedMedia:Register("sound", "|cff00ff98Awoo|r", [[Interface\AddOns\SLUI\Media\Sounds\Awoo.ogg]])
